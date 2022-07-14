1c3dbd61877076f07906b8000d932b1b0672d729
Iara524 <85128293+Iara524@users.noreply.github.com>
2022-07-14 20:10:34 -0300

    direccion

---
+++ Direccion.py
@@ -0,0 +1,10 @@
+Class Direccion:
+    def__init__(self):
+        self.calle = calle
+        self.numero = numero
+        self.ciudad = ciudad
+        self.provincia = provincia
+        self.pais = pais
+
+
+def Validate(direccion):
